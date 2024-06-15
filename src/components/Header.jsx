56ca71f0ae55f0c36b935896d5d4971a69f9e98f
AnyoneOla <133249863+AnyoneOla@users.noreply.github.com>
2024-06-15 12:04:26 +0530

    Delete src directory

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import logo from '../assets/logo.png'
-
-export default function Header(){
-    return (
-        <div>
-            <img className='head-logo' src={logo} />
-        </div>
-    )
-}
